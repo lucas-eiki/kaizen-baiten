e99696b1e6d4d41d09e5d3891e7e2d4274fa0c05
lucas-eiki <lucas.gushiken@sptech.school>
2026-07-09 15:31:38 -0300

    chore: adiciona configuração do backend

---
+++ backend/src/main/java/io/github/lucas_eiki/kaizen_baiten_api/KaizenBaitenApiApplication.java
@@ -0,0 +1,13 @@
+package io.github.lucas_eiki.kaizen_baiten_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KaizenBaitenApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KaizenBaitenApiApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/io/github/lucas_eiki/kaizen_baiten_api/KaizenBaitenApiApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.lucas_eiki.kaizen_baiten_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KaizenBaitenApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
