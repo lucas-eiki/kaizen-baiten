405b501f9dc918e58ea0e8e03748ca08ad7618f2
lucas-eiki <lucas.gushiken@sptech.school>
2026-07-09 20:13:38 -0300

    feat(usuario): adiciona anotações do Lombok

--- backend/src/main/java/io/github/lucas_eiki/kaizen_baiten_api/usuario/model/Usuario.java
+++ backend/src/main/java/io/github/lucas_eiki/kaizen_baiten_api/usuario/model/Usuario.java
@@ -5,12 +5,20 @@ import jakarta.persistence.*;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 import org.hibernate.annotations.CreationTimestamp;
 
 import java.time.LocalDateTime;
 
 @Entity
 @Table(name = "usuario")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class Usuario {
 
     @Id
