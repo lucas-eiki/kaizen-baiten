e24a6e7dd99cc6cc51a7c59e86bc4b88eba7a5d6
lucas-eiki <lucas.gushiken@sptech.school>
2026-07-10 15:28:23 -0300

    feat: mapeia o enum NomePermissao

--- backend/src/main/java/io/github/lucas_eiki/kaizen_baiten_api/permissao/model/Permissao.java
+++ backend/src/main/java/io/github/lucas_eiki/kaizen_baiten_api/permissao/model/Permissao.java
@@ -18,6 +18,7 @@ public class Permissao {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Enumerated(EnumType.STRING)
     @Column(unique = true, nullable = false)
     private NomePermissao nome;
 
